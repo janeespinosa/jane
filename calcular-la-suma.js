2a8ba36dcc46080ae53ecbce82da2796e8b7b570
janeespinosa <168228349+janeespinosa@users.noreply.github.com>
2024-06-18 12:42:35 -0500

    suma

--- calcular-la-suma.js
+++ calcular-la-suma.js
@@ -1,3 +1,8 @@
-let name = ["pepito", "jonh", "joe"];
-
-console.log (name [2]);
+public class Suma {
+  public static void main(string args[]){
+    int numero_uno =5;
+    int numero_dos = 2;
+    unt resultado = 0;
+    resultado = numero_uno + numero_dos;
+    consol.log("el valor de la suma es" + resultado);
+}
