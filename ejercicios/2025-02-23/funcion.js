c233820ca5ed6dd6d36b7630efa143b20032a68c
janeth <janethespinosa0203@gmail.com>
2025-02-23 09:57:42 -0600

    Funcion suma

---
+++ ejercicios/2025-02-23/funcion.js
@@ -0,0 +1,4 @@
+let num1 = parseInt(prompt("Ingrese el primer numero: "));
+let num2 = parseInt(prompt("Ingrese el segundo numero: "));
+let suma = num1 + num2;
+alert("la suma es: " + suma);
--- ejercicios/2025-02-23/variables.js
+++ ejercicios/2025-02-23/variables.js
@@ -1,2 +1,3 @@
 let message;
 message = "Hola";
+alert (message);
