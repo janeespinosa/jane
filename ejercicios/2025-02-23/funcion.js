b16ae460b3d59c26f5e78a4337cefe3702d25b17
janeth <janethespinosa0203@gmail.com>
2025-03-01 14:42:04 -0600

    Modifique la funcion para tratar de ejecutarla

--- ejercicios/2025-02-23/funcion.js
+++ ejercicios/2025-02-23/funcion.js
@@ -1,4 +1,4 @@
-let num1 = parseInt(prompt("Ingrese el primer numero: "));
-let num2 = parseInt(prompt("Ingrese el segundo numero: "));
+let num1 = parseInt("Ingrese el primer numero: ");
+let num2 = parseInt("Ingrese el segundo numero: ");
 let suma = num1 + num2;
-alert("la suma es: " + suma);
+
