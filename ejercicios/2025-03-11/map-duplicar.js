bd3e8ecb1118f5e804d25bdb6f625ff8bb171b52
janeth <janethespinosa0203@gmail.com>
2025-03-11 01:19:51 -0600

    Se triplico el array

--- ejercicios/2025-03-11/map-duplicar.js
+++ ejercicios/2025-03-11/map-duplicar.js
@@ -2,7 +2,7 @@ let array = [1, 2, 3, 4, 5];
 
 // Usar map para duplicar cada elemento del array
 let arrayDuplicado = array.map(function(elemento) {
-    return elemento * 2;
+    return elemento * 3;
 });
 
 console.log(arrayDuplicado);
