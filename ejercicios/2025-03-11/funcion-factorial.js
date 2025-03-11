313298bc6b99be9c61b56ec3e5bbf11c0ca0ff9b
janeth <janethespinosa0203@gmail.com>
2025-03-11 00:57:56 -0600

    Ejercicio de calcular el factorial de un numero, 2025-03-11

---
+++ ejercicios/2025-03-11/funcion-factorial.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+// Ejemplo de uso:
+let numero = 5;
+console.log("El factorial de " ,numero ,"es" ,factorial(numero));
